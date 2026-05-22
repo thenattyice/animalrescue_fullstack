3df1b15e1d8ca28544dae53e5c355d562e1d1239
Nathan Kimborowicz <njkimbo99@hotmail.com>
2026-05-22 08:36:42 -0400

    Upgrade Angular to v19

--- frontend/src/app/components/dogs/dog-edit-modal/dog-edit-modal.component.ts
+++ frontend/src/app/components/dogs/dog-edit-modal/dog-edit-modal.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dog-edit-modal',
-  standalone: true,
-  imports: [],
-  templateUrl: './dog-edit-modal.component.html',
-  styleUrl: './dog-edit-modal.component.css'
+    selector: 'app-dog-edit-modal',
+    imports: [],
+    templateUrl: './dog-edit-modal.component.html',
+    styleUrl: './dog-edit-modal.component.css'
 })
 export class DogEditModalComponent {
 
--- frontend/src/app/components/dogs/dog-list/dog-list.component.ts
+++ frontend/src/app/components/dogs/dog-list/dog-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dog-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './dog-list.component.html',
-  styleUrl: './dog-list.component.css'
+    selector: 'app-dog-list',
+    imports: [],
+    templateUrl: './dog-list.component.html',
+    styleUrl: './dog-list.component.css'
 })
 export class DogListComponent {
 
